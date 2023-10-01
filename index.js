2e7da6dd27d033f76d06f0cc2408ed66b46c59ce
Jaivignesh-afk <23f1001347@ds.study.iitm.ac.in>
2023-10-01 23:12:16 +0530

    handled errors

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var express = require('express');
 const app = express();
 /* GET home page. */
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'public'));
 //app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/",(res,req)=>{
-  res.sendfile(__dirname+"index.html");
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/public/index.html");
 });
 
 app.listen(process.env.PORT||3000,()=>{
