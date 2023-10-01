86ed30dd220e8dc5378c6dfae88a4dda79fbf867
Jaivignesh-afk <23f1001347@ds.study.iitm.ac.in>
2023-10-01 22:39:57 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,15 @@ var express = require('express');
 const app = express();
 /* GET home page. */
 
-app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-const email = req.body.name;
-const password = req.body.password;
-app.get("/",(res,req)=>{
+//app.use(express.static(__dirname+'/public'));
+//app.use(bodyParser.urlencoded({extended: false}));
+
+/*app.get("/",(res,req)=>{
   res.sendFile(__dirname+"index.html");
 });
+*/
 
-app.post("/",(res,req)=>{
-  res.sendFile(__dirname+"Admin.html");
+app.listen(process.env.PORT||3000,()=>{
+  res.send("Server is running succesfully");
 });
-app.get("/",(res,req)=>{
-  
-  if(email=="training@jalaacademy.com" && password=="jobprogram"){
-    //Hi
-  }else{
-    alert("Enter the correct credentials");
-  }
-});
-app.listen(process.env.PORT||3000);
 module.exports = router;
