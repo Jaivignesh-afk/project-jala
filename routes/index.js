99205c1d7e0b4eb63d8bd67c28b2094d25160467
Jaivignesh-afk <23f1001347@ds.study.iitm.ac.in>
2023-10-01 22:56:08 +0530

    sending my home page

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var express = require('express');
 const app = express();
 /* GET home page. */
 
-//app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/public'));
 //app.use(bodyParser.urlencoded({extended: false}));
 
-/*app.get("/",(res,req)=>{
+app.get("/",(res,req)=>{
   res.sendFile(__dirname+"index.html");
 });
-*/
 
 app.listen(process.env.PORT||3000,()=>{
   console.log("Server is running succesfully");
